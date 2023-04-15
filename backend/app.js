c26ae30983f0ec423a2024684c0376fbca6bba2d
monolith-codes <maurice.wessely19@gmail.com>
2023-04-15 04:45:07 +0200

    Add Express JS

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+const express =  require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('hello world'));
+
+app.listen(3000, () => {
+    console.log('My REST API running on port 3000!');
+})
