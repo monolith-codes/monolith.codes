4d02f047790a244b7d76cc6c0717d8273a3b8f69
Maurice Wessely <maurice.wessely19@gmail.com>
2024-04-23 01:28:13 +0200

    update Docker Setup

--- frontend/dev_startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build the Docker image
-docker build -t monolith-frontend-nuxt .
-
-# Run the Docker container
-docker run -p 3000:3000 --name monolith.codes-frontend monolith-frontend-nuxt
